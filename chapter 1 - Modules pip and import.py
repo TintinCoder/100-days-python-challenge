04ddbf654aece54ebcfddaa73c27f4a6c9ebfd6f
TinTin-Coder <divyamyadav40@gmail.com>
2025-09-02 23:42:01 +0530

    Start of chapter

--- chapter 1 - Modules pip and import.py
+++ chapter 1 - Modules pip and import.py
@@ -3,4 +3,4 @@ joke = pyjokes.get_joke()
 
 print(joke)
 
-#This is a comment. It is not executed in a code.
+#This is a comment. It is not executed in a code
---
+++ chapter 1 practice set.py
@@ -0,0 +1,18 @@
+#WAP to print twinkle star poem
+# print("Twinkle twinkle little star, How I wonder what you are")
+
+#WAP to print the table of 5
+# for i in range(11):
+#     print("5 x ", i, "=", 5*i)
+
+#Problem 3
+# import pyjokes
+# joke = pyjokes.get_joke()
+# print(joke)
+
+#Problem 4
+# import os
+# from os import listdir
+# from os.path import isfile, join
+# onlyfiles = [f for f in listdir("C:\Users\Divyam Yadav\Desktop\DIVYAM YADAV\PYTHON 100 DAYS") if isfile(join("C:\Users\Divyam Yadav\Desktop\DIVYAM YADAV\PYTHON 100 DAYS", f))]
+# print(onlyfiles)
