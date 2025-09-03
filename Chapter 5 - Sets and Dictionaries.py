0121b45aeb2aab8bfafdd503965136446f1cb6c0
TinTin-Coder <divyamyadav40@gmail.com>
2025-09-03 22:07:19 +0530

    Started dict

--- Chapter 5 - Sets and Dictionaries.py
+++ Chapter 5 - Sets and Dictionaries.py
@@ -1,1 +1,15 @@
 # CHAPTER 5 - SETS AND DICTIONARIES
+marks = {
+    "Harry": 100,
+    "Shubham": 98,
+    "Avisha": 10
+}
+
+# print(marks, type(marks))
+# print(marks["Harry"])
+
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+marks.update({"Harry": 99, "Divyam": 100})
+print(marks)
