41595d0c3f5ced2d803d8f9248ea13b6ceab1ea8
TinTin-Coder <divyamyadav40@gmail.com>
2025-09-05 00:03:49 +0530

    Completed chapter 5 - Sets and dictionaries

---
+++ Chapter 5 - Practice set.py
@@ -0,0 +1,67 @@
+# Q1 - WAP to create a dictionary for hindi to english translation. Provide user with option to look it up!
+
+# hindi_dict = {
+#     "Namaste": "Hello",
+#     "Kya haal chaal": "How are you",
+#     "Accha": 'Good',
+#     "Bura": "Bad",
+#     "Kursi": "Chair",
+#     "Kutta": "Dog",
+#     "Madad": "help"
+# }
+
+# word = str(input("Enter the word of which the meaning you want: "))
+# print(hindi_dict[word])
+
+
+# Q2 - WAP to input 8 numbers from users and display all unique numbers
+# numbers = set()
+# n1 = int(input('Enter a number: '))
+# numbers.add(n1)
+
+# n2 = int(input('Enter a number: '))
+# numbers.add(n2)
+
+# n3 = int(input('Enter a number: '))
+# numbers.add(n3)
+
+# n4 = int(input('Enter a number: '))
+# numbers.add(n4)
+
+# n5 = int(input('Enter a number: '))
+# numbers.add(n5)
+
+# n6 = int(input('Enter a number: '))
+# numbers.add(n6)
+
+# n7 = int(input('Enter a number: '))
+# numbers.add(n7)
+
+# n8 = int(input('Enter a number: '))
+# numbers.add(n8)
+
+# print(numbers)
+
+
+# Q3 - Create an empty dict
+
+# dick = {}
+# name1 = str(input('Enter your name: '))
+# game1 = str(input("Enter your favourite game: "))
+# name2 = str(input('Enter your name: '))
+# game2 = str(input("Enter your favourite game: "))
+# name3 = str(input('Enter your name: '))
+# game3 = str(input("Enter your favourite game: "))
+# name4 = str(input('Enter your name: '))
+# game4 = str(input("Enter your favourite game: "))
+
+# dick.update({name1: game1, name2: game2, name3: game3, name4:game4})
+# print(dick)
+
+
+
+# Q5 - CHANGE VALUES
+s = {8, 7, 12, 'Harry', [1, 2]}
+print(s)
+
+# Can't update as a list can't be added in a set. 
--- Chapter 5 - Sets and Dictionaries.py
+++ Chapter 5 - Sets and Dictionaries.py
@@ -1,9 +1,9 @@
 # CHAPTER 5 - SETS AND DICTIONARIES
-marks = {
-    "Harry": 100,
-    "Shubham": 98,
-    "Avisha": 10
-}
+# marks = {
+#     "Harry": 100,
+#     "Shubham": 98,
+#     "Avisha": 10
+# }
 
 # print(marks, type(marks))
 # print(marks["Harry"])
@@ -11,5 +11,20 @@ marks = {
 # print(marks.items())
 # print(marks.keys())
 # print(marks.values())
-marks.update({"Harry": 99, "Divyam": 100})
-print(marks)
+# marks.update({"Harry": 99, "Divyam": 100})
+# print(marks['Divyam'])
+# print(marks.get("Shivika")) # This will not show an error and will return none, but indexing it will give an error
+# print(marks["Shivika"]) # This will give an error
+
+# ------------------> SETS <-------------------------------
+
+s ={10, 20, 30, 30}
+e = set() # To make an empty set s = {} will make an empty dictionary
+
+e.add(52)
+e.add(52253)
+e.add(52242)
+print(e)
+
+print(s.intersection(e)) # Will give the common terms in both the sets
+print(s.union(e)) # Will generate a new set containing terms of both the sets
